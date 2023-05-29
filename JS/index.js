9e07b6a0bf9bcacfa5508da0c264bdd78184c8e2
sosso <cypossosmu51c@gmail.com>
2023-05-29 14:25:28 +0200

    JS Base

--- JS/index.js
+++ JS/index.js
@@ -1,2 +1,13 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
+
+canvas.height = innerHeight;
+canvas.width = innerWidth;
+
+const frames = 60;
+const fps = 1000 / frames;
+
+let gameLoop = setInterval(() => {
+    c.fillStyle = "black";
+    c.fillRect(0, 0, canvas.width, canvas.height);
+}, fps);
